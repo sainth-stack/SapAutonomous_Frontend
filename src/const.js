3da329a9827a8f1c7cd3917994ae0f99f85f90b2
Bainocular <BAInocular@seleccionconsulting.com>
2026-05-20 13:48:50 +0530

    Delete src directory

--- src/const.js
+++
@@ -1,5 +0,0 @@
-export const baseURL = 'https://backend-ams-fastapi.cfapps.us10-001.hana.ondemand.com/api'
-// export const baseURL='http://localhost:8000/api'
-
-export const systemMonitoringHistoryURL = 'https://ans-webhook-happy-civet-oc.cfapps.us10-001.hana.ondemand.com/api/history';
-export const batchMonitorURL = 'https://tg-monitoring-backend.cfapps.us10-001.hana.ondemand.com/v1/automate/idoc-data/status';
