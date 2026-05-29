27400736f48f75a791c2f0e0f57d641eac53042c
Bainocular <BAInocular@seleccionconsulting.com>
2026-05-29 19:16:58 +0530

    Add files via upload

---
+++ pages/resource-effectiveness-consultant/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ConsultantWise from '../bi-report/ConsultantWise';
+import './index.css';
+
+const ResourceEffectivenessConsultant = () => {
+  return (
+    <div className="resource-effectiveness-consultant-container">
+  
+      
+      <div className="consultant-section">
+        <ConsultantWise />
+      </div>
+    </div>
+  );
+};
+
+export default ResourceEffectivenessConsultant;
